3b0d186dba4c5186920dbe78a2a1447eca62423f
Jordan Hoare <jordanhoare0@gmail.com>
2024-02-23 18:06:09 +0800

    Create initial bootstraping and set up script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 set -eu
 
+# Determine OS
+OS="$(uname -s)"
+BREW_PATH="/home/linuxbrew/.linuxbrew/bin/brew"
+
 # Install brew (if not already installed)
 if ! command -v brew &> /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    
+    # For Linux, append Homebrew to the current session's PATH
+    if [[ "$OS" == "Linux" ]]; then
+        echo 'eval $($BREW_PATH shellenv)'
+        eval $($BREW_PATH shellenv)
+    fi
 fi
 
-# Install git (if not already installed)
-if ! command -v git &> /dev/null; then
-    brew install git
+# Function to install a package if it's not already installed
+install_package() {
+    local package=$1
+    if ! command -v $package &> /dev/null; then
+        if [[ "$OS" == "Linux" ]]; then
+            $BREW_PATH install $package
+        else
+            brew install $package
+        fi
+    fi
+}
+
+# Install packages
+install_package git
+install_package zsh
+install_package stow
+
+# Change the default shell to Zsh (if not already changed)
+if [ "$SHELL" != "$(which zsh)" ]; then
+    chsh -s $(which zsh)
 fi
 
-# Install zsh (if not already installed)
-if ! command -v zsh &> /dev/null; then
-    brew install zsh
+# Clone the dotfiles to ~/.dotfiles (if not already cloned)
+if [ ! -d "$HOME/.dotfiles" ]; then
+    git clone https://github.com/jordanhoare/dotfiles.git $HOME/.dotfiles
 fi
-
-# Install zsh (if not already installed)
-if ! command -v stow &> /dev/null; then
-    brew install stow
-fi
-
-# Change the default shell to Zsh
-chsh -s $(which zsh)
-
-# Clone the dotfiles to ~/.dotfiles
-git clone https://github.com/jordanhoare/dotfiles.git $HOME/.dotfiles
 
 # Create symbolic links for the configuration files 
 stow --dir=$HOME/.dotfiles/ --target=$HOME zsh
 
+# Change the active shell to Z Shell
 exec zsh
