39511c33f40f158381aa8b263ae9d678d4914522
Jordan Hoare <jordanhoare0@gmail.com>
2024-02-25 19:51:31 +0800

    python installations

--- shell/scripts/python.sh
+++ shell/scripts/python.sh
@@ -5,7 +5,7 @@ set -eu
 if ! command -v pyenv &> /dev/null; then
     git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
 
-    py_versions=("3.8" "3.9" "3.10" "3.11")
+    py_versions=("3.8" "3.10")
     for py_version in "${py_versions[@]}"; do
         $HOME/.pyenv/bin/pyenv install "$py_version"
     done
@@ -13,3 +13,10 @@ fi
 
 # Set the global Python version to the newest one.
 $HOME/.pyenv/bin/pyenv global "${py_versions[-1]}"
+
+# curl -sSL https://install.python-poetry.org | POETRY_HOME=/etc/poetry python3 -
+# # Install poetry
+# if ! command -v poetry &> /dev/null; then
+#     POETRY_HOME="/usr/local/bin/poetry"
+#     curl -sSL https://install.python-poetry.org | python3 -
+
