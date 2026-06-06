4092a30e1744d1dbdc4d2d846179112c25220768
jordanhoare <jordanhoare0@gmail.com>
2026-06-06 15:18:42 +0800

    feat(nix,zed): add fastfetch and switch markdown lsp to marksman
    - add fastfetch to base packages for system info display
    - swap markdown-oxide for marksman (more mature general-purpose markdown lsp)
    - add syntax overrides for catppuccin mocha blur so markdown headers, emphasis, links and inline code render with distinct colours
    - soften button and tab backgrounds to translucent white tints

--- nix/modules/base.nix
+++ nix/modules/base.nix
@@ -33,6 +33,7 @@
     age
     bitwarden-cli
     imagemagick
+    fastfetch
   ];
 
   home.file = {
@@ -52,7 +53,6 @@
     "bin/up".source = ../../bin/up;
     "bin/work".source = ../../bin/work;
     "bin/wallpaper".source = ../../bin/wallpaper;
-    "bin/set-wallpaper".source = ../../bin/set-wallpaper;
 
     ".config/wallpapers/source.jpg".source = ../../config/wallpapers/source.jpg;
     ".config/wallpapers/wallpaper.jpg".source = ../../config/wallpapers/wallpaper.jpg;
